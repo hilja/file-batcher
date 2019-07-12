d7eae786b63c679d1784ddda48a18533826d05ce
hilja <antti.multala@number26.de>
2019-07-12 08:19:27 +0200

    Add update action to bulkEdit

--- src/bulk-edit/index.mjs
+++ src/bulk-edit/index.mjs
@@ -1,11 +1,15 @@
 import { writeSync } from '../write/index.mjs'
 import read from '../read/index.mjs'
+import update from 'immutability-helper'
 
 const bulkEdit = locations => async callback => {
   const allData = await read(locations)
 
   return allData.map(goods => {
-    const actions = { save: writeSync(goods.path) }
+    const actions = {
+      save: writeSync(goods.path),
+      update: target => update(goods, target)
+    }
     const args = { goods, actions }
 
     return callback(args)
