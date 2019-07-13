6c6dfa0b3c63f2db239bfd820a9ba603aaee54a5
hilja <antti.multala@number26.de>
2019-07-13 09:59:10 +0200

    Add async method to the index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import bulkEdit from './src/bulk-edit/index.mjs'
-import read from './src/read/index.mjs'
+import { read, readAsync } from './src/read/index.mjs'
 import remove from './src/remove/index.mjs'
 import { write, writeSync } from './src/write/index.mjs'
 
-export { bulkEdit, read, remove, write, writeSync }
+export { bulkEdit, read, readAsync, remove, write, writeSync }
