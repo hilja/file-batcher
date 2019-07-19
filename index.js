77a2739f336ceec6f4a5ad66b66d7d4c456c6247
hilja <antti.multala@number26.de>
2019-07-19 07:31:52 +0200

    Remove index from bulk edit require string

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.bulkEdit = require('./src/bulk-edit/index')
+module.exports.bulkEdit = require('./src/bulk-edit')
 module.exports.read = require('./src/read')
 module.exports.remove = require('./src/remove')
 module.exports.write = require('./src/write')
