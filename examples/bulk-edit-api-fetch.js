86c413e336a175b3983ba234f2b46011af6abd4f
hilja <antti.multala@number26.de>
2019-07-21 18:23:02 +0200

    Add a bulk edit example

--- examples/bulk-edit-api-fetch.js
+++ examples/bulk-edit-api-fetch.js
@@ -11,17 +11,21 @@ const onEach = async ({ actions }) => {
   // Wait for it
   const dataFromAPI = await fakeFetch
 
-  // Stuff it in to the post. Update is sync.
+  // Stuff it in to the post. Update is sync, btw.
   const newData = update({
     data: { foo: { $set: dataFromAPI } }
   })
 
   // And save it. Save is async.
-  await save(newData)
-  console.log('Just saved this thing \n', newData)
+  try {
+    await save(newData)
+  } catch (error) {
+    console.error(error)
+  }
+  console.log('Just saved this thing:', newData)
 }
 
 const afterAll = () => console.log('All done!')
 
-// Do it two files at the time
+// Do it, say, 2 files at the time. Tweak it so you don't get rate-limited.
 bulkEdit('fixtures/test-content/**', onEach, 2, afterAll)
