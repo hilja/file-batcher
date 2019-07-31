60f8f57583a7d81fd73944431d3db5e48687fa6c
hilja <antti.multala@number26.de>
2019-07-31 07:56:25 +0200

    Better rename example

--- examples/rename-files.js
+++ examples/rename-files.js
@@ -1,7 +1,10 @@
 const { bulkEdit } = require('../index')
 
+const FILE_TO_RENAME = 'fixtures/test-content/articles/test-file-md'
+
 const renameFiles = async ({ actions }) => {
+  // Rename the wrongly names file.
   await actions.rename('test-file.md')
 }
 
-bulkEdit('fixtures/test-content/articles/test-file-md', renameFiles)
+bulkEdit(FILE_TO_RENAME, renameFiles)
