cd7d683273dd9f8321a1cfa4e4f1493064c5d5a4
hilja <antti.multala@number26.de>
2019-12-11 13:19:58 +0100

    Wrap get-path to a try..catch

--- helpers/get-path/index.js
+++ helpers/get-path/index.js
@@ -3,14 +3,17 @@ const path = require('path')
 /**
  * Gets an absolute path to the current working directory.
  *
- * @param {string} file Path to a file or folder.
+ * @param {String} file Path to a file or folder.
+ * @returns {String}
  */
 const getPath = file => {
-  if (!file) {
-    return ''
-  }
+  if (!file) return ''
 
-  return path.isAbsolute(file) ? file : path.resolve(process.env.PWD, file)
+  try {
+    return path.isAbsolute(file) ? file : path.resolve(process.env.PWD, file)
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
 module.exports = getPath
