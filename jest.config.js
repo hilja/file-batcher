3d187a50474576b5079056d3f447f3213b213473
Stefano Cislaghi <s.cislaghi@gmail.com>
2022-08-25 09:19:24 +0100

    chore: dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
+const patterns = '[/\\\\]node_modules[/\\\\](?!aggregate-error|clean-stack|is-path-inside|trash|escape-string-regexp|indent-string|p-map).+\\.(js|jsx)$';
+
 module.exports = {
   verbose: true,
   moduleFileExtensions: ['js', 'json', 'mjs'],
   transform: {
     '^.+.m?js$': 'babel-jest'
   },
+  transformIgnorePatterns: [
+    patterns,
+  ],
   setupFiles: ['<rootDir>/fixtures/jest.init.js']
 }
